7db828c73e08ef72d22d5e61748eab219be348b1
f.c.lopez <casmali.f.lopez@gmail.com>
2018-04-10 09:20:34 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup
+from flask_oauthlib.client import OAuth
+from flask import render_template
+#please work 
+import pprint
+import os
+import json
+from bson.objectid import ObjectId
+import pymongo
+app.debug = True
+ 
+ 
+ if __name__ == '__main__':
+    app.run()
