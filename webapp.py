fa8d98caffe5e4feaabcdd8eed6e13231636b708
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-04-16 09:41:38 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,52 +17,50 @@ oauth = OAuth(app)
 
 #Set up GitHub as OAuth provider
 github = oauth.remote_app(
-    'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
-    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+	'github',
+	consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
+	request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+	base_url='https://api.github.com/',
+	request_token_url=None,
+	access_token_method='POST',
+	access_token_url='https://github.com/login/oauth/access_token',  
+	authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
 @app.context_processor
 def inject_logged_in():
-    return {"logged_in":('github_token' in session)}
+	return {"logged_in":('github_token' in session)}
 
 @app.route('/')
 def home():
-    log = False
-    if 'user_data' in session:
-        log = True
-    #log = False
-    return render_template('home.html', loggedIn = log)
+	log = False
+	if 'user_data' in session:
+		log = True
+	return render_template('home.html', loggedIn = log)
    
 @app.route('/find')
 def findcar():
-    log = False
-    if 'user_data' in session:
-        log = True
-    return render_template('findacar.html', loggedIn = log)
+	log = False
+	if 'user_data' in session:
+		log = True
+	return render_template('findacar.html', loggedIn = log)
    
 @app.route('/account')
 def account():
-    log = False
-    if 'user_data' in session:
-        log = True
-    return render_template('account.html', loggedIn = log)
+	log = False
+	if 'user_data' in session:
+		log = True
+	return render_template('account.html', loggedIn = log)
 def manufacturers():
-  with open('cars.json') as cars_data:
-        cars = json.load(cars_data)
-    options = ""
-    s = []
-    for c in cars:
-        if c['Identification']['Make'] not in s:
-            s.append(c['Identification']['Make'])
-            options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
-    return options
+	with open('cars.json') as cars_data:
+		cars = json.load(cars_data)
+	options = ""
+	s = []
+	for c in cars:
+		if c['Identification']['Make'] not in s:
+		s.append(c['Identification']['Make'])
+		options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+	return options
    
 @app.route('/login')
 def login():   
@@ -93,7 +91,7 @@ def authorized():
 
 @github.tokengetter
 def get_github_oauth_token():
-    return session.get('github_token')
+	return session.get('github_token')
   
 if __name__ == '__main__':
-    app.run()
+	app.run()
