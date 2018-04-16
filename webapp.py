9e606297ad4fdd6c98611726d5fdbcd8cb27151a
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-04-16 09:30:36 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -34,10 +34,10 @@ def inject_logged_in():
 
 @app.route('/')
 def home():
-    #log = False
-   # if 'user_data' in session:
-    #    log = True
     log = False
+    if 'user_data' in session:
+        log = True
+    #log = False
     return render_template('home.html', loggedIn = log)
    
 @app.route('/find')
