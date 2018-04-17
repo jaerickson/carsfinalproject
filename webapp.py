0fa308efec06481f371f64a7fccbe18fe8e1f20c
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-04-17 09:25:06 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, url_for, session, request, jsonify, Markup
 from flask_oauthlib.client import OAuth
 from flask import render_template
-#please work 
 import pprint
 import os
 import json
@@ -13,19 +12,19 @@ app = Flask(__name__)
 app.debug = True #Change this to False for production
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
-oauth = OAuth(app)
+# oauth = OAuth(app)
 
 #Set up GitHub as OAuth provider
-github = oauth.remote_app(
-	'github',
-	consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
-	request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
-	base_url='https://api.github.com/',
-	request_token_url=None,
-	access_token_method='POST',
-	access_token_url='https://github.com/login/oauth/access_token',  
-	authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
-)
+# github = oauth.remote_app(
+# 	'github',
+# 	consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
+# 	request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+# 	base_url='https://api.github.com/',
+# 	request_token_url=None,
+# 	access_token_method='POST',
+# 	access_token_url='https://github.com/login/oauth/access_token',  
+# 	authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+# )
 
 @app.context_processor
 def inject_logged_in():
@@ -88,36 +87,36 @@ def mpg_range():
 	
 	return options
    
-@app.route('/login')
-def login():   
-	return github.authorize(callback=url_for('authorized', _external=True, _scheme='httpS')) #callback URL must match the pre-configured callback URL
+# @app.route('/login')
+# def login():   
+# 	return github.authorize(callback=url_for('authorized', _external=True, _scheme='httpS')) #callback URL must match the pre-configured callback URL
 
-@app.route('/logout')
-def logout():
-	session.clear()
-	return render_template('message.html', message='You were logged out')
+# @app.route('/logout')
+# def logout():
+# 	session.clear()
+# 	return render_template('message.html', message='You were logged out')
 
-@app.route('/login/authorized')
-def authorized():
-	resp = github.authorized_response()
-	if resp is None:
-		session.clear()
-		message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
-	else:
-		try:
-			session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
-			session['user_data']=github.get('user').data
-			message='You were successfully logged in as ' + session['user_data']['login']
-		except Exception as inst:
-			session.clear()
-			print(inst)
-			message='Unable to login, please try again.  '
-	return render_template('message.html', message=message)
+# @app.route('/login/authorized')
+# def authorized():
+# 	resp = github.authorized_response()
+# 	if resp is None:
+# 		session.clear()
+# 		message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+# 	else:
+# 		try:
+# 			session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
+# 			session['user_data']=github.get('user').data
+# 			message='You were successfully logged in as ' + session['user_data']['login']
+# 		except Exception as inst:
+# 			session.clear()
+# 			print(inst)
+# 			message='Unable to login, please try again.  '
+# 	return render_template('message.html', message=message)
     
 
-@github.tokengetter
-def get_github_oauth_token():
-	return session.get('github_token')
+# @github.tokengetter
+# def get_github_oauth_token():
+# 	return session.get('github_token')
   
 if __name__ == '__main__':
 	app.run()
