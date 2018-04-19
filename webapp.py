74c544000cbfc64b5e2e98ff4072b6349e8d05a8
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-04-19 10:02:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -107,7 +107,7 @@ def city_range_options():
    
 @app.route('/login')
 def login():   
- 	return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+ 	return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
