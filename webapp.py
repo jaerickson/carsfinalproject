f872088c1102d98f41fd8fcbc1b95b2de1029f2e
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-04-26 10:11:26 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 import pymongo
-import Flask-pymongo
+#import Flask-pymongo
 from flask import Flask, redirect, url_for, session, request, jsonify, Markup
 from flask_oauthlib.client import OAuth
 from flask import render_template
