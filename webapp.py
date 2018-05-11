31948c3763f1e0e1f897d72bc02d5f864d9a89c7
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-05-11 09:10:48 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -57,7 +57,8 @@ def findcar():
 	if 'user_data' in session:
 		log = True
 	if 'vehicle' in request.args:
-		print(request.args['q6'][1])
+		print("this is working")
+		print(request.args['q6'])
 		
 	return render_template('findacar.html', loggedIn = log, manufacturers = manufacturers_options(), fuel_type = fuel_type_options(), cylinder=cylinder_options(), forward_gears=forward_gears_options())
    
