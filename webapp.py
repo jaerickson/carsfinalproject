8f63f52bec1dc2eff1bbc36b0a364b50d72b9508
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-05-14 09:58:11 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -86,11 +86,8 @@ def account():
 									l = i["Dimensions"]["Length"] / 12
 									h = i["Dimensions"]["Height"] / 12
 									v = w*l*h
-									if (request.args['q7'] == "small" && v <= 130)||(request.args['q7'] == "medium" && (v > 130 && v < 160))||(request.args["q7'] == "large" && v > 160)         
-										
-								
-							
-							num = 674677
+									if (request.args['q7'] == "small" && v <= 130)||(request.args['q7'] == "medium" && (v > 130 && v < 160))||(request.args["q7'] == "large" && v > 160):
+										num = 838
 	return render_template('account.html', loggedIn = log, username =  session['user_data']['login'])
 
 def manufacturers_options():
