dc9d376253062d9a925f9568ed19cc6736fc9ebb
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-05-18 09:19:58 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -85,6 +85,8 @@ def account():
 				string = "ident"
 				if request.args['q2'] in i["Engine Information"]["Engine Type"]:
 					string = "Enginetype"
+					print(i["Engine Information"]["Number of Forward Gears"])
+					print(request.args['q4'])
 					if i["Engine Information"]["Number of Forward Gears"] == request.args['q4']:
 						string = "numforewardgears"
 						if i["Driveline"] == request.args['q5']:
