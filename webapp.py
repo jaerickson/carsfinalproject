113fc8bd21a55cfd8937642f7e9cb6096f541632
jaerickson <31251673+jaerickson@users.noreply.github.com>
2018-05-21 09:28:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -79,8 +79,6 @@ def account():
 		string = "got this far"
 		for i in cars:
 			string = "aaaa"
-			print(i["Identification"]["Classification"])
-			print(request.args['q1'])
 			if i["Identification"]["Classification"] == request.args['q1']:
 				string = "ident"
 				if request.args['q2'] in i["Engine Information"]["Engine Type"]:
