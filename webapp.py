09dcc16d104b0bbcbe791d2463aa7f654776f9e1
kebudke <31251507+kebudke@users.noreply.github.com>
2018-06-05 00:44:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -114,8 +114,10 @@ def account():
 			username = session['user_data']['login']
 			if collection.find_one({username:{"$exists": True}}) is not None:
 				returns = username + ": " + collection.find_one({username:{"$exists": True}})[username]
-			t = "Here are the results from your quiz on : "  + s
-			collection.update_one({session['user_data']['login']: {"$exists": True}},{"$set":{session['user_data']['login']:str(t)}},upsert=True)
+				t = "Here are the results from your quiz on : "  + s
+				collection.update_one({session['user_data']['login']: {"$exists": True}},{"$set":{session['user_data']['login']:str(t)}},upsert=True)
+			else:
+				collection.insert({ session['user_data']['login']: "bar" })
 	return render_template('account.html', loggedIn = log, new_results =  r, prev_results = returns)
 
 def manufacturers_options():
